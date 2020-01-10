600beddfa4566dc942d7f8838ce501a0f9b566d5
Manzulia Ruslan <43026067+gloooman@users.noreply.github.com>
2020-01-10 19:47:51 +0200

    Add files

---
+++ orders/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.views.generic.edit import FormMixin
+from django.views.generic import TemplateView
+from products.models import Product
+from .models import *
+from .cart import Cart
+from .forms import *
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            product=product,
+            quantity=cd['quantity'],
+            update_quantity=cd['update']
+        )
+
+    return JsonResponse(request.POST)
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    print(dir(cart.session))
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return JsonResponse({
+        'product_id': product.id
+    })
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
+    return render(request, 'cart/detail.html', {'cart': cart})
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                ProductInOrder.objects.create(order=order,
+                                              product=item['product'],
+                                              price=item['price'],
+                                              quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', {'cart': cart, 'form': form})
+
---
+++ static/js/test.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// let person = prompt("How are you?", 'human');
+// let isTrue = confirm('It`s True?')
+// alert(`You are ${person}`);
+// if (isTrue) {
+//     alert("Amassing")
+// }   else if (-isTrue){
+//     alert("why?")
+// }   else {
+//     alert('try again')
+// }
+
+
+// Тернарный оперетор
+// let result = условие ? значение1 : значение;
+
+// let age = prompt('Ваш возраст:', 18);
+// let message = (age < 3) ? "Здраствуй, малыш!":
+//     (age < 18) ? "Привет!":
+//     (age < 3) ? "Здраствуйте!":
+//     "Какой необычный возраст";
+// alert(message);
+
+
+// Function Declaration: функция в основном потоке кода
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
+// Function Expression: функция как часть выражения
+// let sum = function(a, b) {
+//   let result = a + b;
+//   return result;
+// };
+// Стрелочные функции:
+// выражение в правой части
+// let sum = (a, b) => a + b;
+// многострочный код в фигурных скобках { ... }, здесь нужен return:
+// let sum = (a, b) => {
+//   return a + b;
+// };
+// без аргументов
+// let sayHi = () => alert("Привет");
+// с одним аргументом
+// let double = n => n * 2;
+
+
