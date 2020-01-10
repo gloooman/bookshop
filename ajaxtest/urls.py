da6df98500aeb4f6b535259e44966897effba687
Manzulia Ruslan <43026067+gloooman@users.noreply.github.com>
2020-01-10 19:36:57 +0200

    Revert "push"
    This reverts commit 798cc1417c4e45917425466889bc6fe5985aa4ca.

--- ajaxtest/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import *
-
-urlpatterns = [
-    url(r'^signup/$', SignUpView.as_view(), name='signup'),
-    url(r'^ajax/validate_username/$', validate_username, name='validate_username'),
-]
