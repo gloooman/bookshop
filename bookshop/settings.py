86f54f332c48da8bb3dc0338c2bc8e6b12655d2f
Manzulia Ruslan <43026067+gloooman@users.noreply.github.com>
2020-01-26 21:10:37 +0200

    fix settings.py

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,3 +147,4 @@ INTERNAL_IPS = [
 ]
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
