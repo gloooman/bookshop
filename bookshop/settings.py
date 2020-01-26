24128bcf33036fe29a0e103cf0ddc3a30f5a1d4c
Manzulia Ruslan <43026067+gloooman@users.noreply.github.com>
2020-01-26 22:23:33 +0200

    fix search

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!^=51_nh0n%#%nm))a73--4c74tqx!_3fg!7&3z(4@e2&ml(p=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['protected-basin-82325.herokuapp.com', '*']
 
