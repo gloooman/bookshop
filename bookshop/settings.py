b5e0adb9d5adf056ad09ce654d0a4e33ce309d28
Manzulia Ruslan <43026067+gloooman@users.noreply.github.com>
2020-01-26 22:15:35 +0200

    fix settings.py

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_prod/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_prod")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static_prod"),
 )
 # MEDIA_ROOT = os.path.join(STATIC_URL, "media")
 # MEDIA_URL = '/media/'
